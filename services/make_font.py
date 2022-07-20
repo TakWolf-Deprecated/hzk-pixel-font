b0f48a91e2cade22d3cc5b193e0f7dac7d23f824
TakWolf <takwolf@foxmail.com>
2022-07-20 19:16:25 +0800

    更新工具类

--- services/make_font.py
+++ services/make_font.py
@@ -15,45 +15,45 @@ dot_em_units = 100
 outputs_dir = 'outputs/'
 
 
-def _collect_design_files(design_dirs):
+def _collect_glyph_files(glyphs_dirs):
     """
-    收集可用字母表，生成设计文件映射表
+    收集可用字母表，生成字形源文件映射表
     """
     alphabet = set()
-    design_file_paths = {}
-    for design_dir in design_dirs:
-        if not os.path.isdir(design_dir):
+    glyph_file_paths = {}
+    for glyphs_dir in glyphs_dirs:
+        if not os.path.isdir(glyphs_dir):
             continue
-        for design_file_parent_dir, _, design_file_names in os.walk(design_dir):
-            for design_file_name in design_file_names:
-                if not design_file_name.endswith('.png'):
+        for glyph_file_dir, _, glyph_file_names in os.walk(glyphs_dir):
+            for glyph_file_name in glyph_file_names:
+                if not glyph_file_name.endswith('.png'):
                     continue
-                design_file_path = os.path.join(design_file_parent_dir, design_file_name)
-                uni_hex_name = design_file_name.replace('.png', '')
+                glyph_file_path = os.path.join(glyph_file_dir, glyph_file_name)
+                uni_hex_name = glyph_file_name.replace('.png', '')
                 if uni_hex_name == 'notdef':
-                    design_file_paths['.notdef'] = design_file_path
+                    glyph_file_paths['.notdef'] = glyph_file_path
                 else:
                     code_point = int(uni_hex_name, 16)
-                    design_file_paths[code_point] = design_file_path
+                    glyph_file_paths[code_point] = glyph_file_path
                     alphabet.add(chr(code_point))
     alphabet = list(alphabet)
     alphabet.sort(key=lambda c: ord(c))
-    return alphabet, design_file_paths
+    return alphabet, glyph_file_paths
 
 
 def _convert_point_to_open_type(point, ascent):
     """
-    转换左上角坐标系为 OpenType 坐标系
+    转换左上角原点坐标系为 OpenType 坐标系
     """
     x, y = point
     y = ascent - y
     return x, y
 
 
-def _draw_glyph(design_file_path, ascent, is_ttf):
-    logger.info(f'draw glyph {design_file_path}')
-    design_data, width, _ = glyph_util.load_design_data_from_png(design_file_path)
-    outlines = glyph_util.get_outlines_from_design_data(design_data, dot_em_units)
+def _draw_glyph(glyph_file_path, ascent, is_ttf):
+    logger.info(f'draw glyph {glyph_file_path}')
+    glyph_data, width, _ = glyph_util.load_glyph_data_from_png(glyph_file_path)
+    outlines = glyph_util.get_outlines_from_glyph_data(glyph_data, dot_em_units)
     if is_ttf:
         pen = TTGlyphPen(None)
     else:
@@ -80,7 +80,7 @@ def _draw_glyph(design_file_path, ascent, is_ttf):
         return pen.getCharString(), advance_width
 
 
-def _create_font_builder(name_strings, vertical_metrics, glyph_order, character_map, design_file_paths, is_ttf):
+def _create_font_builder(name_strings, vertical_metrics, glyph_order, character_map, glyph_file_paths, is_ttf):
     units_per_em, ascent, descent, x_height, cap_height = vertical_metrics
     builder = FontBuilder(units_per_em, isTTF=is_ttf)
     builder.setupNameTable(name_strings)
@@ -88,9 +88,9 @@ def _create_font_builder(name_strings, vertical_metrics, glyph_order, character_
     builder.setupCharacterMap(character_map)
     glyphs = {}
     advance_widths = {}
-    glyphs['.notdef'], advance_widths['.notdef'] = _draw_glyph(design_file_paths['.notdef'], ascent, is_ttf)
+    glyphs['.notdef'], advance_widths['.notdef'] = _draw_glyph(glyph_file_paths['.notdef'], ascent, is_ttf)
     for code_point, glyph_name in character_map.items():
-        glyphs[glyph_name], advance_widths[glyph_name] = _draw_glyph(design_file_paths[code_point], ascent, is_ttf)
+        glyphs[glyph_name], advance_widths[glyph_name] = _draw_glyph(glyph_file_paths[code_point], ascent, is_ttf)
     if is_ttf:
         builder.setupGlyf(glyphs)
         horizontal_metrics = {glyph_name: (advance_width, glyphs[glyph_name].xMin) for glyph_name, advance_width in advance_widths.items()}
@@ -140,8 +140,8 @@ def run():
             'description': 'HZK pixel font.',
             'vendorURL': 'https://hzk-pixel-font.takwolf.com',
         }
-        alphabet, design_file_paths = _collect_design_files([
-            f'assets/design/{px}/',
+        alphabet, glyph_file_paths = _collect_glyph_files([
+            f'assets/glyphs/{px}/',
             f'outputs/png/asc/{px}/',
             f'outputs/png/hzk/{px}/',
         ])
@@ -152,13 +152,13 @@ def run():
             glyph_name = f'uni{code_point:04X}'
             glyph_order.append(glyph_name)
             character_map[code_point] = glyph_name
-        otf_builder = _create_font_builder(name_strings, vertical_metrics, glyph_order, character_map, design_file_paths, False)
+        otf_builder = _create_font_builder(name_strings, vertical_metrics, glyph_order, character_map, glyph_file_paths, False)
         otf_builder.save(os.path.join(outputs_dir, f'hzk-pixel-{px}px.otf'))
         logger.info(f'make {px}px otf')
         otf_builder.font.flavor = 'woff2'
         otf_builder.save(os.path.join(outputs_dir, f'hzk-pixel-{px}px.woff2'))
         logger.info(f'make {px}px woff2')
-        ttf_builder = _create_font_builder(name_strings, vertical_metrics, glyph_order, character_map, design_file_paths, True)
+        ttf_builder = _create_font_builder(name_strings, vertical_metrics, glyph_order, character_map, glyph_file_paths, True)
         ttf_builder.save(os.path.join(outputs_dir, f'hzk-pixel-{px}px.ttf'))
         logger.info(f'make {px}px ttf')
         _make_preview_image_file(px)
