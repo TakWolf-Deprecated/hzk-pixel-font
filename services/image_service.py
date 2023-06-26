2e1e12d31102f3f7dfeb2b0df9660f30c08cd218
TakWolf <takwolf@foxmail.com>
2023-06-26 19:14:40 +0800

    调整示例文案

--- services/image_service.py
+++ services/image_service.py
@@ -13,10 +13,10 @@ def make_preview_image_file(font_config: FontConfig):
     font = ImageFont.truetype(os.path.join(path_define.outputs_dir, f'{font_config.outputs_name}.woff2'), font_config.size)
     text_color = (0, 0, 0, 255)
 
-    image = Image.new('RGBA', (font_config.size * 35, font_config.size * 11), (255, 255, 255, 255))
+    image = Image.new('RGBA', (font_config.size * 27, font_config.size * 11), (255, 255, 255, 255))
     draw = ImageDraw.Draw(image)
     draw.text((font_config.size, font_config.size), '汉字库像素字体 / HZK Pixel Font', fill=text_color, font=font)
-    draw.text((font_config.size, font_config.size * 3), '我们每天度过的称之为日常的生活，其实是一个个奇迹的连续也说不定。', fill=text_color, font=font)
+    draw.text((font_config.size, font_config.size * 3), '我们度过的每个平凡的日常，也许就是连续发生的奇迹。', fill=text_color, font=font)
     draw.text((font_config.size, font_config.size * 5), 'THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.', fill=text_color, font=font)
     draw.text((font_config.size, font_config.size * 7), 'the quick brown fox jumps over a lazy dog.', fill=text_color, font=font)
     draw.text((font_config.size, font_config.size * 9), '0123456789', fill=text_color, font=font)
