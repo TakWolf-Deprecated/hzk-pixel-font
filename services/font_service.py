51b7eb60f3891a2d9b9cfc63c111e7eb6b76b83a
TakWolf <takwolf@foxmail.com>
2023-07-26 22:28:01 +0800

    调整字体格式构建逻辑

--- services/font_service.py
+++ services/font_service.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from pixel_font_builder import FontBuilder, Glyph, StyleName, SerifMode, WidthMode
+from pixel_font_builder.opentype import Flavor
 
 from configs import path_define, FontConfig
 from utils import fs_util, glyph_util
@@ -66,17 +67,19 @@ def make_font_files(font_config: FontConfig, character_mapping: dict[int, str], 
     fs_util.make_dirs(path_define.outputs_dir)
 
     builder = _create_builder(font_config, character_mapping, glyph_file_paths)
-    otf_builder = builder.to_otf_builder()
+
     otf_file_path = os.path.join(path_define.outputs_dir, f'{font_config.outputs_name}.otf')
-    otf_builder.save(otf_file_path)
+    builder.save_otf(otf_file_path)
     logger.info("Make font file: '%s'", otf_file_path)
-    otf_builder.font.flavor = 'woff2'
+
     woff2_file_path = os.path.join(path_define.outputs_dir, f'{font_config.outputs_name}.woff2')
-    otf_builder.save(woff2_file_path)
+    builder.save_otf(woff2_file_path, flavor=Flavor.WOFF2)
     logger.info("Make font file: '%s'", woff2_file_path)
+
     ttf_file_path = os.path.join(path_define.outputs_dir, f'{font_config.outputs_name}.ttf')
     builder.save_ttf(ttf_file_path)
     logger.info("Make font file: '%s'", ttf_file_path)
+
     bdf_file_path = os.path.join(path_define.outputs_dir, f'{font_config.outputs_name}.bdf')
     builder.save_bdf(bdf_file_path)
     logger.info("Make font file: '%s'", bdf_file_path)
