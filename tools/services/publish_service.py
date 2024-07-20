daa29aad22f9fa7fec79a286a295e23989718453
TakWolf <takwolf@foxmail.com>
2024-07-21 02:00:26 +0800

    调整 update_docs 函数

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -1,20 +1,18 @@
+import re
 import shutil
-from pathlib import Path
 
 from loguru import logger
 
-from tools import configs
 from tools.configs import path_define
 
 
-def _copy_file(file_name: str, from_dir: Path, to_dir: Path):
-    from_path = from_dir.joinpath(file_name)
-    to_path = to_dir.joinpath(file_name)
-    shutil.copyfile(from_path, to_path)
-    logger.info("Copy from '{}' to '{}'", from_path, to_path)
-
-
 def update_docs():
-    path_define.docs_dir.mkdir(exist_ok=True)
-    for font_config in configs.font_configs:
-        _copy_file(font_config.preview_image_file_name, path_define.outputs_dir, path_define.docs_dir)
+    for file_dir, _, file_names in path_define.outputs_dir.walk():
+        for file_name in file_names:
+            if re.match(r'preview-.*px\.png', file_name) is None:
+                continue
+            path_from = file_dir.joinpath(file_name)
+            path_to = path_define.docs_dir.joinpath(path_from.relative_to(path_define.outputs_dir))
+            path_to.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copyfile(path_from, path_to)
+            logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
